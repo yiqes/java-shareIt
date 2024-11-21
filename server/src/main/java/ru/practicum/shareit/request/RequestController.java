0f05ed0e70d2972c08036ec0df0b62502ccc90b7
yiqes <155768368+yiqes@users.noreply.github.com>
2024-11-21 12:45:41 +0300

    исправление checkstyle

--- server/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.request;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
